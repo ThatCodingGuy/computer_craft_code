89c59f061408155c3b32348d3d13abf951a74972
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-21 15:09:03 -0500

    Adding scan_area function

--- turboCo/movement.lua
+++ turboCo/movement.lua
@@ -67,3 +67,51 @@ function figure_out_facing()
     print("Robot can't move, refuel or unstuck")
     return
 end
+
+
+function scan_area(width, depth, block_callback)
+    -- +width to the right, -width to the left
+    -- +depth forward, -depth backwards
+    -- block_callback is called with block data whenever a collision occurs.
+
+    local direction = figure_out_facing()
+    if not direction then
+        print("Could not determine facing")
+        return
+    end
+
+    local start_x, start_y, start_z = gps.locate()
+    if not start_x then
+        print("Could not connect to gps")
+        return
+    end
+
+    local x_total = 0
+    local z_total = 0
+
+    if direction == NORTH then
+        x_total = -width
+        z_total = depth
+    end
+
+    if direction == SOUTH then
+        x_total = -depth
+        z_total = width
+    end
+
+    if direction == EAST then
+        x_total = width
+        z_total = -depth
+    end
+
+    if direction == WEST then
+        x_total = depth
+        z_total = width
+    end
+
+    local final_x = start_x + x_total
+    local final_z = start_y + z_total
+    print("Scanning from "..start_x..","..start_z.." to "..x_total..","..z_total)
+    
+
+end

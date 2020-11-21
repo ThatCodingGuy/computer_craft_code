45b7a21010c025c3d5298be663e1fe4a6745b692
Pier-Luc Michaud <pluc.m@outlook.com>
2020-11-20 22:41:28 -0500

    ilu mat

---
+++ pluc/miner.lua
@@ -0,0 +1,31 @@
+for i = 0, 100 do
+    turtle.refuel(2)
+    for y = 0, 50 do
+        turtle.dig()
+        turtle.forward()
+    end
+    turtle.digUp()
+    turtle.up()
+    turtle.turnLeft()
+    turtle.turnLeft()
+    for y = 0, 50 do
+        turtle.dig()
+        turtle.forward()
+    end
+    turtle.down()
+    turtle.turnRight()
+    turtle.dig()
+    turtle.forward()
+    turtle.turnRight()
+
+    dropAllButOne(1)
+    dropAllButOne(2)
+    dropAllButOne(3)
+    dropAllButOne(4)
+    dropAllButOne(5)
+end
+
+function dropAllButOne(slot)
+    turtle.select(slot)
+    turtle.drop(turtle.getItemCount() - 1)
+ end

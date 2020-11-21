6a7b656b294bee311a13588970a339abf1d0bcb6
Olivier <oliviertrahan>
2020-11-21 11:40:28 -0500

    monitor features

--- turboCo/monitor.lua
+++ turboCo/monitor.lua
@@ -1,11 +1,38 @@
 -- This file is intended to provide extensions to the terminal (term) API of computercraft
 -- The intention is to create commonly used utility functions that computercraft somehow does not provide
 
---Sets cursor to the beggining of the next line after writing
-function writeLn(screen, text)
-  screen.write(text)
+local function setCursorToNextLine(screen)
   local x,y = screen.getCursorPos()
   screen.setCursorPos(1, y+1)
+end
+
+--Assuming that you have only one monitor peripheral, returns the only one
+function getInstance()
+  return peripheral.find("monitor")
+end
+
+--Write so that the text wraps to the next line
+function write(screen, text)
+  local width,height = screen.getSize()
+  remainingText = text
+  while string.len(remainingText) > 0 do
+    local x,y = screen.getCursorPos()
+    local remainingX = width - x + 1
+    remainingLineText = string.sub(remainingText, 1, remainingX)
+    screen.write(remainingLineText)
+    x,y = screen.getCursorPos()
+    if (x == width) then
+      setCursorToNextLine(screen)
+    end
+    remainingText = string.sub(remainingText, remainingX + 1, -1)
+  end
+  screen.write(text)
+end
+
+--Sets cursor to the beggining of the next line after writing
+function writeLn(screen, text)
+  write(screen, text)
+  setCursorToNextLine(screen)
 end
 
 --Writes centered text for a monitor of any size, then enter a new line
@@ -21,11 +48,9 @@ function writeCenterLn(screen, text)
   writeLn(screen, text)
 end
 
---Writes centered text for a monitor of any size, then enter a new line
+-- Clears the screen then resets the cursor pointer
 function clear(screen)
   screen.clear()
   screen.setCursorPos(1,1)
 end
-
-function getInstance
 

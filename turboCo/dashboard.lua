145d2ab5d711efb581fbed2971dad6628b358e7f
Pier-Luc Michaud <pluc.m@outlook.com>
2020-11-21 16:27:10 -0500

    <3

--- turboCo/dashboard.lua
+++ turboCo/dashboard.lua
@@ -12,6 +12,10 @@ end
 
 function updateRobot()
     local x, y, z = gps.locate()
+    print("Name=" .. os.getComputerLabel())
+    print("X=" .. x)
+    print("Y=" .. y)
+    print("Z=" .. z)
     http.request
     {
         url = "https://turboco-app.azurewebsites.net/api/robots/" .. os.getComputerLabel(),

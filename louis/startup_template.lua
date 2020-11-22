857d65d3d99a05e58f264bfd641499ed3793cbc2
wasalp <louis.parent@gmail.com>
2020-11-21 21:17:02 -0500

    y

--- louis/startup_template.lua
+++ louis/startup_template.lua
@@ -2,8 +2,13 @@
 os.loadAPI("gitlib/turboCo/importManager.lua")
 importManager.ImportRequirements("gitlib/louis/requirements")
 cobblerbot.getBearings()
+local nextCheck = os.epoch("utc") + (1000*60*1)
+
 while (true)
 do
-    importManager.CheckForUpdate()
+    if os.epoch("utc") > nextCheck then
+        importManager.CheckForUpdate()
+        nextCheck = os.epoch("utc") + (1000*60*1)
+    end
     cobblerbot.miningCycle()
 end

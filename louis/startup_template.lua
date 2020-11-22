a59c8805eed091129acbe670656c734d5ce5f015
wasalp <louis.parent@gmail.com>
2020-11-21 21:30:05 -0500

    y

--- louis/startup_template.lua
+++ louis/startup_template.lua
@@ -6,6 +6,10 @@ local nextCheck = os.epoch("utc") + (1000*60*1)
 
 while (true)
 do
+    print("**********")
+    print(nextCheck)
+    print(os.epoch("utc"))
+    print("===========")
     if os.epoch("utc") > nextCheck then
         importManager.CheckForUpdate()
         nextCheck = os.epoch("utc") + (1000*60*1)

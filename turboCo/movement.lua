f352fddac3fc3f65128274739eb5114317bd04d5
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-21 22:52:13 -0500

    Fix logging

--- turboCo/movement.lua
+++ turboCo/movement.lua
@@ -136,12 +136,15 @@ function pathfind(start, destination, map)
 
     print("Pathfind "..start.."/"..destination)
     for key, value in pairs(map) do
-        print(key, " ", value)
+        print("Map ", key, " ", value)
     end
     
 
     while #queue > 0 do
         local path = table.remove(queue, 1)
+        for key, value in pairs(visited) do
+            print("Visited ", key, " ", value)
+        end
 
         -- Check foward, backward, left, right, up, down
         local last_elem = path[#path]
@@ -166,9 +169,6 @@ function pathfind(start, destination, map)
                 return new_path
             end
 
-            for key, value in pairs(visited) do
-                print("Visisted ", key, " ", value)
-            end
             if map[target] and not visited[target] then
                 local new_path = copy(path)
                 table.insert(new_path, target)

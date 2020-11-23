6212bd65bd995fb1532434cd2f379d3dd0face9a
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-23 13:57:11 -0500

    prints'

--- turboCo/movement.lua
+++ turboCo/movement.lua
@@ -360,6 +360,7 @@ function explore_area(area, block_callback)
     print("Pre-adjacent")
     local adjacent = get_adjacent_blocks(position)
     for k in pairs(adjacent) do print(k) end
+    print("Post-adjacent")
     local function is_in_area(x) print(x); return area[x] end
     local function is_not_explored(x) return not explored[x] end
     adjacent = filter(adjacent, is_in_area)

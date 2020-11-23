2be14d3eec7ffec93e52df8d5fb88992aadd5a33
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-23 15:00:55 -0500

    Save

--- turboCo/movement.lua
+++ turboCo/movement.lua
@@ -401,10 +401,9 @@ function explore_area(area, block_callback)
                 local path = pathfind_with_map(position, target, walkable_map) 
                 print("Found path")
                 for i = 1, #path, 1 do
-                    print("Following path "..position.." to "..node)
-                    facing, position = visit_adjacent(position, node, facing, block_callback)
+                    facing, position = visit_adjacent(position, path[i], facing, block_callback)
                     if not node == position then
-                        facing, position = visit_adjacent(position, node, facing, force_dig)
+                        facing, position = visit_adjacent(position, path[i], facing, force_dig)
                     end
                 end
             end

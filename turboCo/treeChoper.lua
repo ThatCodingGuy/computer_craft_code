781e39b9d3298f9f67d4e48b7e1626389ff63277
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-26 15:29:02 -0500

    Add tree chopper

---
+++ turboCo/treeChoper.lua
@@ -0,0 +1,73 @@
+
+os.loadAPI("/gitlib/turboCo/movement.lua")
+
+
+
+function treeChop(block_data, position, adjacent, facing, direction)
+
+    local start_position = position
+    local start_facing = facing
+
+    if block_data.name == "minecraft:birch_log" then
+        -- Trees grow up to 7 high, with a width of 5
+        -- Tree is in adjacent
+
+        local tree_x, tree_y, tree_z = movement.split_coord(adjacent)
+
+        local tree_area = {}
+        for x = tree_x - 2, tree_x + 2, 1 do
+            for z = tree_z - 2, tree_z + 2, 1 do
+                for y = tree_z, tree_z+7, 1 do
+                    tree_area[coord(x, y, z)] = 1
+                end
+            end
+        end
+
+
+
+        facing, position = movement.explore_area(tree_area, position, facing, movement.force_dig)
+        facing, position = movement.navigate(position, facing, start_position)
+        movement.turn_to_face(facing, start_facing)
+        facing = start_facing
+    end
+
+    sleep(1)
+
+    return facing, position
+end
+
+function run(refuel_coords, tree_spot)
+    -- Slot 1 is for saplings
+
+    local facing = figure_out_facing()
+    if not direction then
+        error("Could not determine facing")
+        return
+    end
+
+    local start_x, start_y, start_z = gps.locate()
+    if not start_x then
+        error("Could not connect to gps")
+        return
+    end
+    local current = coord(start_x, start_y, start_z)
+
+
+    -- We always position ourselves to the south
+    local turtle_x, turtle_y, turtle_z = movement.split_coord(tree_spot)
+    local turtle_spot = movement.coord(turtle_x + 1, turtle_y, turtle_z)
+
+
+    facing, current = movement.navigate_to(current, facing, refuel_coords)
+    movement.refuel()
+
+
+    while true do
+        facing, current = movement.navigate_to(current, facing, turtle_spot)
+
+        local tree = {}
+        table.insert(tree, tree_spot)
+        movement.explore_area(tree, current, facing, movement.keepChurning(refuel_coords, tree_chop))
+        
+    end
+end

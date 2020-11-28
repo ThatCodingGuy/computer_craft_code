22cf2fe78d7e17ff338178efeb565845ab1c11d0
Leo Denault <leo.denault@gmail.com>
2020-11-28 01:06:30 -0500

    Make a class function to replace the constructor function.
    This allows callers more flexibility by giving them a structure for
    defining private and public members.

--- testing/turboCo/lua_helpers_test.lua
+++ testing/turboCo/lua_helpers_test.lua
@@ -1,53 +1,75 @@
 local lua_helpers = require("turboCo.lua_helpers")
+local class = lua_helpers.class
+local enum = lua_helpers.enum
 
 describe("Lua helpers", function()
-    describe("constructor", function()
-        local Prototype = {
-            a = 0,
-            b = "something"
-        }
+    describe("class", function()
 
-        it("should return a new instance when invoked without parameters", function()
-            Prototype.new = lua_helpers.constructor
+        it("should return a valid prototype with constructor", function()
+            local MyClass = class({
+                static_var = 123,
+                static_fun = function()
+                    return 5
+                end
+            }, function(a)
+                local self = {
+                    b = 2
+                }
 
-            local instance1 = Prototype:new()
-            local instance2 = Prototype:new()
-            instance1.a = 10
-            instance1.b = "something else"
-            instance2.a = 20
-            instance2.b = "more things"
+                local function incrementA()
+                    a = a + 1
+                end
 
-            assert.are.equal(0, Prototype.a)
-            assert.are.equal("something", Prototype.b)
-            assert.are.equal(10, instance1.a)
-            assert.are.equal("something else", instance1.b)
-            assert.are.equal(20, instance2.a)
-            assert.are.equal("more things", instance2.b)
-        end)
+                local function incrementB()
+                    self.b = self.b + 1
+                end
+                return {
+                    getA = function()
+                        return a
+                    end,
+                    getB = function()
+                        return self.b
+                    end,
+                    incrementA = incrementA,
+                    incrementB = incrementB
+                }
+            end)
 
-        it("should execute init function when invoked with parameters", function()
-            function Prototype:new(a, b)
-                return lua_helpers.constructor(Prototype, function(self)
-                    self.a = a
-                    self.b = b
-                end)
-            end
+            local instance1 = MyClass.new(3)
+            local instance2 = MyClass.new(10)
+            local a11 = instance1.getA()
+            local b11 = instance1.getB()
+            local a21 = instance2.getA()
+            local b21 = instance2.getB()
+            instance1.incrementA()
+            instance1.incrementB()
+            instance2.incrementA()
+            instance2.incrementB()
+            local a12 = instance1.getA()
+            local b12 = instance1.getB()
+            local a22 = instance2.getA()
+            local b22 = instance2.getB()
 
-            local instance1 = Prototype:new(10, "something else")
-            local instance2 = Prototype:new(20, "more things")
-
-            assert.are.equal(0, Prototype.a)
-            assert.are.equal("something", Prototype.b)
-            assert.are.equal(10, instance1.a)
-            assert.are.equal("something else", instance1.b)
-            assert.are.equal(20, instance2.a)
-            assert.are.equal("more things", instance2.b)
+            assert.are.equal(123, MyClass.static_var)
+            assert.are.equal(5, MyClass.static_fun())
+            assert.are.equal(nil, instance1.static_var)
+            assert.are.equal(nil, instance1.static_fun)
+            assert.are.equal(nil, instance2.static_var)
+            assert.are.equal(nil, instance2.static_fun)
+            assert.are.equal(3, a11)
+            assert.are.equal(10, a21)
+            assert.are.equal(4, a12)
+            assert.are.equal(11, a22)
+            assert.are.equal(2, b11)
+            assert.are.equal(2, b21)
+            assert.are.equal(3, b12)
+            assert.are.equal(3, b22)
         end)
     end)
 
     describe("enum", function()
         it("should return same table with values in acsending order", function()
-            local FakeEnum = lua_helpers.enum {
+            local FakeEnum = enum {
                 "ONE", "TWO", "THREE"
             }
 
--- turboCo/lua_helpers.lua
+++ turboCo/lua_helpers.lua
@@ -2,27 +2,24 @@
 
 local lua_helpers = {}
 
---- Allows creating a constructor for a class.
--- @param class The prototype instance used as the base for instances of a class.
--- @param init An optional function that runs setup code as part of the constructor. The function
--- provides a single parameter that represents the newly-constructed object.
--- @return The instance of the class.
-function lua_helpers.constructor(class, init)
-    local object_metatable = {}
-    setmetatable(object_metatable, class)
-    class.__index = class
-
-    if (init) then init(object_metatable) end
-
-    return object_metatable
+--- Allows creating a class.
+-- @param statics A table containing static definitions for the class.
+-- @param definition The definition of the class, expressed as a function. The function may take any
+-- number of parameters as its invocation acts as the constructor. The function must return a table
+-- containing the public members of the class.
+--
+function lua_helpers.class(statics, definition)
+    statics.new = definition
+    return statics
 end
 
---- Allows defining enums without specifyng their values.
+--- Allows defining enums without specifying their values.
 -- This will automatically assign integer values to the enums in ascending order starting from 1
 -- onwards.
 -- @param values The table of names to give to the enums. Specify this as a simple table of strings
 -- without assigning them values.
 -- @return The original enum table, `values`, with its values assigned.
+--
 function lua_helpers.enum(values)
     local num_values = #values
     for i = 1, num_values do

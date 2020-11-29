69acd6e397597bec1fc6d9a6fca35e448b3301ef
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-28 22:12:58 -0500

    Save

--- turboCo/modem.lua
+++ turboCo/modem.lua
@@ -9,7 +9,7 @@ table.insert(directions, "top")
 table.insert(directions, "bottom")
 
 function openModems()
-    for i = 0, #directions, 1 do
+    for i = 1, #directions, 1 do
         if peripheral.getType(directions[i]) == "modem" then
             rednet.open(directions[i])
         end
@@ -17,7 +17,7 @@ function openModems()
 end
 
 function closeModems()
-    for i = 0, #directions, 1 do
+    for i = 1, #directions, 1 do
         if peripheral.getType(directions[i]) == "modem" then
             rednet.close(directions[i])
         end

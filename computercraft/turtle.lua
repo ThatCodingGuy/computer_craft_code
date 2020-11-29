4e01dfea53bf2fcc71509ad7001e16989f3793da
Leo Denault <leo.denault@gmail.com>
2020-11-28 23:25:06 -0500

    Create initial mocking implementation for ComputerCraft APIS.

---
+++ computercraft/turtle.lua
@@ -0,0 +1,88 @@
+return {
+    craft = function(quantity)
+    end,
+    forward = function()
+    end,
+    back = function()
+    end,
+    up = function()
+    end,
+    down = function()
+    end,
+    turnLeft = function()
+    end,
+    turnRight = function()
+    end,
+    select = function(slotNum)
+    end,
+    getSelectedSlot = function()
+    end,
+    getItemCount = function(slotNum)
+    end,
+    getItemSpace = function(slotNum)
+    end,
+    getItemDetail = function(slotNum)
+    end,
+    equipLeft = function()
+    end,
+    equipRight = function()
+    end,
+    attack = function(toolSide)
+    end,
+    attackUp = function(toolSide)
+    end,
+    attackDown = function(toolSide)
+    end,
+    dig = function(toolSide)
+    end,
+    digUp = function(toolSide)
+    end,
+    digDown = function(toolSide)
+    end,
+    place = function(signText)
+    end,
+    placeUp = function()
+    end,
+    placeDown = function()
+    end,
+    detect = function()
+    end,
+    detectUp = function()
+    end,
+    detectDown = function()
+    end,
+    inspect = function()
+    end,
+    inspectUp = function()
+    end,
+    inspectDown = function()
+    end,
+    compare = function()
+    end,
+    compareUp = function()
+    end,
+    compareDown = function()
+    end,
+    compareTo = function(slot)
+    end,
+    drop = function(count)
+    end,
+    dropUp = function(count)
+    end,
+    dropDown = function(count)
+    end,
+    suck = function(amount)
+    end,
+    suckUp = function(amount)
+    end,
+    suckDown = function(amount)
+    end,
+    refuel = function(quantity)
+    end,
+    getFuelLevel = function()
+    end,
+    getFuelLimit = function()
+    end,
+    transferTo = function(slot, quantity)
+    end,
+}

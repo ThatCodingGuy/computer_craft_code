f53f2cd07f038854008e85ce1d90f392c91f1b0b
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-29 22:22:09 -0500

    Save

--- turboCo/elevator.lua
+++ turboCo/elevator.lua
@@ -5,12 +5,12 @@ while true do
 
     if y < 120 then
         local delta = 200 - y
-        for i = 1, i < delta, 1 do
+        for i = 1, delta, 1 do
             turtle.up()
         end
     else
         local delta = y - 63
-        for i = delta, i > 0, -1 do
+        for i = delta, 0, -1 do
             turtle.down()
         end
     end

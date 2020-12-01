cab728b5e2b60e66f66495cbf86c61500fe3efa3
Mathieu Jobin <thatcodingguy@gmail.com>
2020-11-30 20:13:27 -0500

    Save

--- turboCo/server/warehouse.lua
+++ turboCo/server/warehouse.lua
@@ -12,16 +12,29 @@ modem.openModems()
 --          Calls the withdrawal API
 
 
--- db contains a map of item label -> {
---    "storage_chests": [
---        "total": int
---        "position": coord
---    ]
--- 
+
+-- Tables
+-- item_name -> {
+--    chest_data : [{
+--        position: coord, quantity: int
+--    }]
 -- }
 
+local corner1 = movement.coord(-96, 63, 464)
+local corner2 = movement.coord(-96, 63, 435)
+local corner3 = movement.coord(-153, 63, 435)
+local corner4 = movement.coord(-153, 63, 464)
 
-local db = {}
+local max_y = 100
+local min_y = 63
+
+local max_x = -96
+local min_x = -153
+
+local max_z = 464
+local min_z = 435
+
+local chest_chest = movement.coord(-94, 63, 445)
 
 local data
 if fs.exists("/db") then
@@ -32,8 +45,73 @@ else
     data = {}
 end
 
+local chest_spots = {}
+for y = min_y, max_y, 1 do
+    for x = min_x, max_x, 1 do
+        for z = min_z, max_z, 1 do
+            local spot = movement.coord(x, y, z)
+            if x % 2 == 0 and z % 2 == 0 do
+                chest_spots[spot] = 0
+            end
+        end
+    end
+end
+
+
+for item_name, storage_data in pairs(data) do
+    for i = 1, #storage_data["chests"], 1 do
+        local spot = storage_data["chests"][i]["position"]
+        chest_spots[spot] = 1
+    end
+end
+
+local function save_db()
+    local db = fs.open("/db", w)
+    local raw_data = textutils.serializeJSON(data)
+    db.write(raw_data)
+end
+
+local function get_next_chest_spot()
+    local shortest_distance = 9999999999
+    local best_spot = nil
+    for position, is_taken in pairs(chest_spots) do
+        if not is_taken then
+            local distance = movement.distance(chest_chest, position)
+            if distance < shortest_distance then
+                best_spot = position
+            end
+        end
+    end
+
+    return best_spot
+end
+
+local function init_item_entry(item_name)
+    print("New item registered: "..item_name)
+    data[item_name] = {}
+    data[item_name]["chest_data"] = {}
+end
+
+local function dispatch_chest(position, item_name)
+    print("Dispatching new chest to "..position)
+    chest_spots[position] = 1
+
+    local chest_data = {}
+    chest_data["position"] = position
+    chest_data["quantity"] = 0
+
+    table.insert(data[item_name]["chest_data"], chest_data)
+
+    -- TODO: Send the robot
+end
+
 
 local function deposit(item_name, quantity)
-    
+    if not data[item_name] then
+        local spot = get_next_chest_spot()
+        dispatch_chest(spot)
+    end
+
+    save_db()
 end
 

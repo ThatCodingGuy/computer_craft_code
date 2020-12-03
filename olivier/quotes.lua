7c0b1abbba355ce5a55f5b652d745bc9aabe1925
Olivier <oliviertrahan>
2020-12-03 01:15:25 -0500

    fix

--- olivier/quotes.lua
+++ olivier/quotes.lua
@@ -108,22 +108,23 @@ function getNextQuotesAndSwitchPage()
     pageNumber = pageNumber + 1
   end
   if not pageViewManager.hasNextPage() then
-    if pageCounterContent == nil then
-      pageCounterContent = ScreenContent.create{
-        screenBuffer=screenBottomBuffer,
-        screenBufferWriteFunc=screenBottomBuffer.writeCenter,
-        text=createPageTrackerString(),
-        textColor=colors.gray,
-        bgColor=colors.lightBlue
-      }
-    end
     local newPageScreenBuffer = ScreenBuffer.createFullScreenFromTopAndBottom{screen=screen, topOffset=3, bottomOffset=1}
     local newPage = Page.create{screenBuffer=newPageScreenBuffer}
     pageViewManager.addPage(newPage)
     writeQuotes(newPageScreenBuffer)
   end
   pageViewManager.switchToNextPage()
-  pageCounterContent.updateText(createPageTrackerString())
+  if pageCounterContent == nil then
+    pageCounterContent = ScreenContent.create{
+      screenBuffer=screenBottomBuffer,
+      screenBufferWriteFunc=screenBottomBuffer.writeCenter,
+      text=createPageTrackerString(),
+      textColor=colors.gray,
+      bgColor=colors.lightBlue
+    }
+  else
+    pageCounterContent.updateText(createPageTrackerString())
+  end
 end
 
 local screenBottomBuffer = ScreenBuffer.createFullScreenAtBottomWithHeight{screen=screen, height=1}

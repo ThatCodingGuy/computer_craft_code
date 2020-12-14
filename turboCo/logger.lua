399a83594afd8840f10bafc506333d7dadff88f4
Leo Denault <leo.denault@gmail.com>
2020-12-13 22:03:14 -0500

    Upgrade logger code.
    *   When logging, clients can choose what level to log with.
    *   When running an application, clients have the option to set the log
    filter level. This will allow to filter out unwanted (i.e. DEBUG)
    messages
    *   When running an application, clients may set the output printing
    function, which currently defaults to Lua's print function

--- turboCo/logger.lua
+++ turboCo/logger.lua
@@ -1,5 +1,49 @@
--- simple wrapper around print in case we want to do central log management --
+local lua_helpers = dofile("./gitlib/turboCo/lua_helpers.lua")
 
-function log(msg)
-    print(msg)
-end
+local class = lua_helpers.class
+local enum = lua_helpers.enum
+
+local LoggingLevel = enum {
+    "DEBUG", "INFO", "WARNING", "ERROR"
+}
+local print_to_output = print
+local log_level_filter = LoggingLevel.WARNING
+
+--- A simple logger that allows writing messages to stdout.
+-- @param print_to_stdout An optional function that prints the contents to whatever output should be
+-- used.
+Logger = class({
+    LoggingLevel = LoggingLevel,
+    print_to_output = print_to_output,
+    log_level_filter = log_level_filter,
+
+}, function()
+    local function debug(message)
+        if Logger.log_level_filter <= Logger.LoggingLevel.DEBUG then
+            Logger.print_to_output("D: " .. message)
+        end
+    end
+    local function info(message)
+        if Logger.log_level_filter <= Logger.LoggingLevel.INFO then
+            Logger.print_to_output("I: " .. message)
+        end
+    end
+    local function warn(message)
+        if Logger.log_level_filter <= Logger.LoggingLevel.WARNING then
+            Logger.print_to_output("W: " .. message)
+        end
+    end
+    local function error(message)
+        if Logger.log_level_filter <= Logger.LoggingLevel.ERROR then
+            Logger.print_to_output("E: " .. message)
+        end
+    end
+
+    return {
+        info = info,
+        warn = warn,
+        error = error,
+    }
+end)
+
+return Logger

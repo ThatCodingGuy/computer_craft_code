5693ba2fc9b3cd7e1bf987ee143f8925de1a7040
Leo Denault <leo.denault@gmail.com>
2020-12-15 00:05:28 -0500

    Allow blocking on the amount of time remaining in a recurring task.

--- tests/turboCo/recurring_task_test.lua
+++ tests/turboCo/recurring_task_test.lua
@@ -10,13 +10,14 @@ describe("Recurring task", function()
     end
 
     before_each(function()
+        clock_time = 0
         os = test_setup.generate_cc_mocks(mock).os
         os.clock = function()
             return clock_time
         end
     end)
 
-    it("Should yield when timer not expired", function()
+    it("should yield when timer not expired", function()
         local task_performed_count = 0
         local task = RecurringTask.new(100, function()
             task_performed_count = task_performed_count + 1
@@ -29,7 +30,7 @@ describe("Recurring task", function()
         assert.are.equal(1, task_performed_count)
     end)
 
-    it("Should execute task when timer expires", function()
+    it("should execute task when timer expires", function()
         local task_performed_count = 0
         local task = RecurringTask.new(100, function()
             task_performed_count = task_performed_count + 1
@@ -40,5 +41,42 @@ describe("Recurring task", function()
         task.update()
 
         assert.are.equal(2, task_performed_count)
+    end)
+
+    it("should wait correct amount of time when blocking on task", function()
+        local sleep_calls = {}
+        _G.sleep = function(time_to_sleep)
+            table.insert(sleep_calls, time_to_sleep)
+        end
+        local task = RecurringTask.new(100, function()
+        end)
+
+        task.wait_until_update()
+        assert.are.equal(0, #sleep_calls)
+        task.wait_until_update()
+        assert.are.equal(0, #sleep_calls)
+        task.update()
+
+        task.wait_until_update()
+        assert.are.equal(1, #sleep_calls)
+        assert.are.equal(100, sleep_calls[1])
+        advance_clock(100)
+
+        task.wait_until_update()
+        assert.are.equal(1, #sleep_calls)
+
+        advance_clock(100)
+        task.wait_until_update()
+        assert.are.equal(1, #sleep_calls)
+        task.update()
+
+        advance_clock(50)
+        task.wait_until_update()
+        assert.are.equal(2, #sleep_calls)
+        assert.are.equal(50, sleep_calls[2])
+        advance_clock(50)
+
+        task.wait_until_update()
+        assert.are.equal(2, #sleep_calls)
     end)
 end)
--- turboCo/recurring_task.lua
+++ turboCo/recurring_task.lua
@@ -7,16 +7,28 @@ local class = lua_helpers.class
 -- @param perform_task The task that should be executed periodically.
 RecurringTask = class({}, function(interval, perform_task)
     local self = {
+        next_execution_details = {
+            remaining_time = 0,
+            last_clock_read = 0
+        },
         task = coroutine.create(function()
             perform_task()
             local start_time = os.clock()
             while true do
                 local end_time = os.clock()
-                if end_time - start_time < interval then
-                    coroutine.yield()
+                local delta = end_time - start_time
+                if delta < interval then
+                    coroutine.yield {
+                        remaining_time = interval - delta,
+                        last_clock_read = end_time
+                    }
                 else
                     perform_task()
                     start_time = os.clock()
+                    coroutine.yield {
+                        remaining_time = interval,
+                        last_clock_read = start_time
+                    }
                 end
             end
         end)
@@ -24,13 +36,35 @@ RecurringTask = class({}, function(interval, perform_task)
 
     --- Starts the timer on this task.
     local function start()
-        coroutine.resume(self.task)
+        local values = { coroutine.resume(self.task) }
+        self.next_execution_details = values[2]
+    end
+
+    --- Blocks on this task until it's ready to update.
+    local function wait_until_update()
+        local current_time = os.clock()
+        local time_since_last_clock_read = current_time - self.next_execution_details.last_clock_read
+        local remaining_time = self.next_execution_details.remaining_time
+        if time_since_last_clock_read < remaining_time then
+            local sleep_time = remaining_time - time_since_last_clock_read
+            sleep(sleep_time)
+            print(current_time + sleep_time)
+            self.next_execution_details = {
+                remaining_time = 0,
+                last_clock_read = current_time + sleep_time
+            }
+        end
+        self.next_execution_details = {
+            remaining_time = remaining_time - time_since_last_clock_read,
+            last_clock_read = current_time
+        }
     end
 
     return {
         start = start,
         --- Updates this task and possibly executes its logic.
         update = start,
+        wait_until_update = wait_until_update,
     }
 end)
 

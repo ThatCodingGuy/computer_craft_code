89e4a87323441f1d3930a7c32d23e22fa1037baf
Leo Denault <leo.denault@gmail.com>
2020-12-19 02:23:30 -0500

    Update feeder.lua to use updated infrastructure.
    *   Also add debug messages

--- leo/feeder.lua
+++ leo/feeder.lua
@@ -1,4 +1,4 @@
-local EventHandler = dofile("./gitlib/turboCo/event/eventHandler.lua")
+local events = dofile("./gitlib/turboCo/event/events.lua")
 local inventory = dofile("./gitlib/carlos/inventory.lua")
 local Logger = dofile("./gitlib/turboCo/logger.lua")
 local RecurringTask = dofile("./gitlib/turboCo/recurring_task.lua")
@@ -9,44 +9,73 @@ local FOOD_CHECK_PERIOD = 30 -- 30 seconds
 local FOOD_LABEL = "minecraft:wheat"
 local DELTAS_TO_STOP_FEEDING = 10
 local logger = Logger.new()
+local inform_food_strategy
+local check_for_more_food
+local detect_food_outages
 
-local function get_food_count()
-    return inventory.countItemWithName(FOOD_LABEL)
+local function has_food()
+    local food_left = inventory.countItemWithName(FOOD_LABEL)
+    logger.debug("There is ", food_left, " food left.")
+    return food_left > 0
 end
 
 local function wait_for_food()
-    if get_food_count() > 0 then
-        return
-    end
-
-    logger.info("Ran out of food. Waiting for more...")
-    while get_food_count() <= 0 do
-        os.sleep(FOOD_CHECK_PERIOD)
+    if not has_food() then
+        logger.warn("Waiting for food before feeding cows.")
+        while not has_food() do
+            events.sleep(FOOD_CHECK_PERIOD)
+        end
     end
 end
 
 local function feed_cows()
-    if get_food_count() <= 0 then
-        logger.warn("Should have fed cows, but no food was available. Abandoning.")
-        return
-    end
-
+    wait_for_food()
     logger.info("Feeding cows.")
     local failed_placements = 0
     while failed_placements < DELTAS_TO_STOP_FEEDING do
         inventory.selectItemWithName(FOOD_LABEL)
         if turtle.placeDown() then
-            failed_placements = failed_placements + 1
-        else
             failed_placements = 0
+            logger.debug("Succeeded in feeding.")
+        else
+            failed_placements = failed_placements + 1
+            logger.debug("Failed to feed ", failed_placements, " times.")
         end
     end
-    wait_for_food()
+    logger.debug("Done feeding.")
+end
+
+check_for_more_food = function()
+    logger.warn("Ran out of food.")
+    while not has_food() do
+        events.wait_for_inventory_change()
+    end
+    inform_food_strategy = detect_food_outages
+end
+
+detect_food_outages = function()
+    logger.info("Detected food.")
+    while has_food() do
+        events.wait_for_inventory_change()
+    end
+    inform_food_strategy = check_for_more_food
+end
+
+local function inform_food()
+    while true do
+        inform_food_strategy()
+    end
 end
 
 local function run()
     Logger.log_level_filter = Logger.LoggingLevel.INFO
-    RecurringTask.new(COW_BREED_COOLDOWN, feed_cows, EventHandler.create()).run()
+    local feed_cows_task = RecurringTask.new(COW_BREED_COOLDOWN, feed_cows)
+    if has_food() then
+        inform_food_strategy = detect_food_outages
+    else
+        inform_food_strategy = check_for_more_food
+    end
+    parallel.waitForAll(feed_cows_task.run, inform_food)
 end
 
 run()

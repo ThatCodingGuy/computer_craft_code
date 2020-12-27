f46dfdc3fbdddc70c2c412f8cea531295bb78bfb
Leo Denault <leo.denault@gmail.com>
2020-12-26 22:19:24 -0500

    Pass definitions to prepasses.

--- tests/turboCo/app/cli_argument_prepass_test.lua
+++ tests/turboCo/app/cli_argument_prepass_test.lua
@@ -1,32 +1,35 @@
-local CliArgumentParser = dofile("./gitlib/turboCo/app/cli_argument_parser.lua")
 local CliArgumentPrepass = dofile("./gitlib/turboCo/app/cli_argument_prepass.lua")
 
 describe("CLI argument prepass", function()
     it("should run handlers after parsing", function()
+        local handler_1_defs
+        local handler_2_defs
         local handler_1_args
         local handler_2_args
         local handlers = {
-            handler_1 = function(arguments)
+            handler_1 = function(definitions, arguments)
+                handler_1_defs = definitions
                 handler_1_args = arguments
             end,
-            handler_2 = function(arguments)
+            handler_2 = function(definitions, arguments)
+                handler_2_defs = definitions
                 handler_2_args = arguments
             end,
         }
+        local definitions = { {
+                                  long_name = "arg_1"
+                              } }
 
-        local parsed_arguments = CliArgumentPrepass.new(
-                CliArgumentParser.new {
-                    {
-                        long_name = "arg_1"
-                    }
-                }, {
-                    handlers.handler_1,
-                    handlers.handler_2,
-                })                                 .parse {
+        local parsed_arguments = CliArgumentPrepass.new(definitions, {
+            handlers.handler_1,
+            handlers.handler_2,
+        })                                         .parse {
             "--arg_1=qwerty",
             "positional"
         }
 
+        assert.are.same(definitions, handler_1_defs)
+        assert.are.same(definitions, handler_2_defs)
         assert.are.same(parsed_arguments, handler_1_args)
         assert.are.same(parsed_arguments, handler_2_args)
     end)
--- turboCo/app/cli_argument_prepass.lua
+++ turboCo/app/cli_argument_prepass.lua
@@ -1,16 +1,21 @@
+local CliArgumentParser = dofile("./gitlib/turboCo/app/cli_argument_parser.lua")
 local lua_helpers = dofile("./gitlib/turboCo/lua_helpers.lua")
 
 local class = lua_helpers.class
 
---- A CLI argument parser that uses `parser` to parse arguments and then runs each handler in
--- `prepass_handlers` on the parsed arguments.
-local CliArgumentPrepass = class({}, function(parser, prepass_handlers)
+--- A CLI argument parser that parses CLI arguments and then runs each handler in `prepass_handlers`
+-- on the parsed arguments.
+local CliArgumentPrepass = class({}, function(definitions, prepass_handlers)
+    local self = {
+        parser = CliArgumentParser.new(definitions)
+    }
+
     --- Parses the CLI arguments within `args`, runs the prepass handlers, and then returns the
     -- parsed arguments. See CliArgumentParser for details on the return value.
     local function parse(args)
-        local parsed_arguments = parser.parse(args)
+        local parsed_arguments = self.parser.parse(args)
         for _, handle in ipairs(prepass_handlers) do
-            handle(parsed_arguments)
+            handle(definitions, parsed_arguments)
         end
         return parsed_arguments
     end

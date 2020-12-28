09c61e9ff00ac0794c56e95718d498099fefc3cd
Leo Denault <leo.denault@gmail.com>
2020-12-27 23:58:30 -0500

    Update feeder script to use the event handler.

--- leo/feeder.lua
+++ leo/feeder.lua
@@ -1,3 +1,4 @@
+local EventHandler = dofile("./gitlib/turboCo/event/eventHandler.lua")
 local events = dofile("./gitlib/turboCo/event/events.lua")
 local inventory = dofile("./gitlib/carlos/inventory.lua")
 local Logger = dofile("./gitlib/turboCo/logger.lua")
@@ -9,10 +10,13 @@ local number_def = common_argument_definitions.number_def
 
 --- Controls a turtle to feed whatever items it has in its hands to animals below it.
 local COW_BREED_COOLDOWN_DEFAULT = 60 * 5.5 -- 5 minutes 30 seconds
+local CHECK_FOOD_COOLDOWN_DEFAULT = 1 -- 1 second
 local FOOD_LABEL = "minecraft:wheat"
 local DELTAS_TO_STOP_FEEDING = 10
 local cow_breed_cooldown = COW_BREED_COOLDOWN_DEFAULT
+local check_food_cooldown = CHECK_FOOD_COOLDOWN_DEFAULT
 local logger = Logger.new()
+local event_handler = EventHandler.create()
 local inform_food_strategy
 local check_for_more_food
 local detect_food_outages
@@ -30,10 +34,6 @@ local function wait_for_food()
 end
 
 local function feed_cows()
-    if not has_food() then
-        logger.warn("Waiting for food before feeding cows.")
-        wait_for_food()
-    end
     logger.info("Feeding cows.")
     local failed_placements = 0
     while failed_placements < DELTAS_TO_STOP_FEEDING do
@@ -49,24 +49,42 @@ local function feed_cows()
     logger.debug("Done feeding.")
 end
 
+local function attempt_to_feed_cows()
+    if has_food() then
+        feed_cows()
+        return
+    end
+    logger.warn("Waiting for food before feeding cows.")
+
+    local handle_id
+    handle_id = event_handler.addHandle("turtle_inventory", function()
+        if has_food() then
+            event_handler.removeHandle(handle_id)
+            feed_cows()
+        end
+    end)
+end
+
 check_for_more_food = function()
     logger.warn("Ran out of food.")
-    wait_for_food()
-    inform_food_strategy = detect_food_outages
+    local handle_id
+    handle_id = event_handler.addHandle("turtle_inventory", function()
+        if has_food() then
+            event_handler.removeHandle(handle_id)
+            detect_food_outages()
+        end
+    end)
 end
 
 detect_food_outages = function()
     logger.info("Detected food.")
-    while has_food() do
-        events.wait_for_inventory_change()
-    end
-    inform_food_strategy = check_for_more_food
-end
-
-local function inform_food()
-    while true do
-        inform_food_strategy()
-    end
+    local handle_id
+    handle_id = event_handler.addHandle("turtle_inventory", function()
+        if not has_food() then
+            event_handler.removeHandle(handle_id)
+            check_for_more_food()
+        end
+    end)
 end
 
 local function run()
@@ -81,15 +99,20 @@ local function run()
     if parsed_arguments.breed_cooldown ~= nil then
         cow_breed_cooldown = parsed_arguments.breed_cooldown
     end
-    logger.info("Will feed cows every " .. cow_breed_cooldown .. " seconds.")
+    logger.info(
+            "Will feed cows every "
+                    .. cow_breed_cooldown
+                    .. " seconds and check food every"
+                    .. check_food_cooldown
+                    .. "seconds.")
 
-    local feed_cows_task = RecurringTask.new(cow_breed_cooldown, feed_cows)
     if has_food() then
-        inform_food_strategy = detect_food_outages
+        detect_food_outages()
     else
-        inform_food_strategy = check_for_more_food
+        check_for_more_food()
     end
-    parallel.waitForAny(inform_food, feed_cows_task.run)
+    event_handler.scheduleRecurring(attempt_to_feed_cows, cow_breed_cooldown)
+    event_handler.pullEvents()
 end
 
 run()

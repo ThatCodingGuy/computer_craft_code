ab0099d9b19fc0560d88f12f3824872bdfc31bb8
Leo Denault <leo.denault@gmail.com>
2020-12-28 21:24:06 -0500

    Update the fuel station manager server component to make use of the newer event handler API.

--- turboCo/server/fuel/fuel_station_manager.lua
+++ turboCo/server/fuel/fuel_station_manager.lua
@@ -1,34 +1,28 @@
 local modem = dofile("./gitlib/turboCo/modem.lua")
 local Logger = dofile("./gitlib/turboCo/logger.lua")
+local common_argument_parsers = dofile("./gitlib/turboCo/app/common_argument_parsers.lua")
+local common_argument_definitions = dofile("./gitlib/turboCo/app/common_argument_definitions.lua")
+local EventHandler = dofile("./gitlib/turboCo/event/eventHandler.lua")
 local ObservableValue = dofile("./gitlib/turboCo/observable_value.lua")
-local RecurringTask = dofile("./gitlib/turboCo/recurring_task.lua")
 local FuelCoordParser = dofile("./gitlib/turboCo/server/fuel/fuel_coord_parser.lua")
 local FuelStationGroup = dofile("./gitlib/turboCo/server/fuel/fuel_station_group.lua")
 
-Logger.log_level_filter = Logger.LoggingLevel.INFO
-local logger = Logger.new()
-local fuel_station_coordinate_file_name = arg[1]
+local number_def = common_argument_definitions.number_def
 
-if fuel_station_coordinate_file_name == nil then
-    logger.error("Expected a file name containing the station coordinates passed as argument.")
-    return
-end
+local PROTOCOL = "fuel_station"
 
-modem.openModems()
-
-local protocol = "fuel_station"
-rednet.host(protocol, "fuel_station_host")
-
+local fuel_request
+local fuel_done
+local router = {
+    refuel = fuel_request,
+    refuel_done = fuel_done,
+}
 local observable_station_coords = ObservableValue.new()
 local stations = FuelStationGroup.new(
         80 * 64 --[[Assumes that a stack of coal/charcoal is being used to refuel.]],
         observable_station_coords)
-local fuel_coord_parser = FuelCoordParser.new(fuel_station_coordinate_file_name)
-local parser_task = RecurringTask.new(60, function()
-    observable_station_coords.set_value(fuel_coord_parser.parse())
-end)
 
-local function fuel_request(sender_id, request)
+function fuel_request(sender_id, request)
     local nearest = stations.find_nearest(request["position"])
 
     if nearest then
@@ -44,35 +38,61 @@ local function fuel_request(sender_id, request)
     return response
 end
 
-local function fuel_done(sender_id, request)
+function fuel_done(sender_id, request)
     stations.release(sender_id)
     local response = {}
     response["status"] = "success"
     return response
 end
 
-local function receive()
-    while true do
-        senderId, message = rednet.receive(protocol, 10)
-        if senderId then
-            return senderId, message
-        end
+local function handle_request(event_data)
+    local _, senderId, message, protocol = unpack(event_data)
+    if senderId == nil or protocol ~= PROTOCOL then
+        return
     end
+
+    local request = textutils.unserializeJSON(message)
+    local request_type = request["type"]
+    local response = router[request_type](senderId, request)
+    local serialized_response = textutils.serializeJSON(response)
+    rednet.send(senderId, serialized_response, PROTOCOL)
 end
 
 function run()
-    while true do
-        local senderId, message = receive()
-        local request = textutils.unserializeJSON(message)
-        local request_type = request["type"]
-        local response = router[request_type](senderId, request)
-        local message = textutils.serializeJSON(response)
-        rednet.send(senderId, message, protocol)
+    local argument_parser = common_argument_parsers.default_parser {
+        {
+            long_name = "coord_file_name",
+            short_name = "f",
+            description = "The name of the file containing the fuel station coordinates. This file "
+                    .. "will be read periodically to keep the server's in-memory coordinates up to "
+                    .. "date. This argument is required.",
+        },
+        number_def {
+            long_name = "coord_refresh_period",
+            short_name = "r",
+            description = "The amount of time, in seconds, to wait before reloading the coordinate "
+                    .. "file. The default is 60 seconds.",
+            default = 60,
+        },
+    }
+    local parsed_arguments = argument_parser.parse(arg)
+    local logger = Logger.new()
+    if parsed_arguments.coord_file_name == nil then
+        logger.error("Please specify a file name containing fuel station coordinates using the "
+                .. "--coord_file_name/-f parameter.")
+        return
     end
+
+    modem.openModems()
+    rednet.host(PROTOCOL, "fuel_station_host")
+
+    local fuel_coord_parser = FuelCoordParser.new(parsed_arguments.coord_file_name)
+    local event_handler = EventHandler.create()
+    event_handler.scheduleRecurring(function()
+        observable_station_coords.set_value(fuel_coord_parser.parse())
+    end, parsed_arguments.coord_refresh_period)
+    event_handler.addHandle("rednet_message", handle_request)
+    event_handler.pullEvents()
 end
 
-local router = {}
-router["refuel"] = fuel_request
-router["refuel_done"] = fuel_done
-
-parallel.waitForAll(parser_task.run, run)
+run()

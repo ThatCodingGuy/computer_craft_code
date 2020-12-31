91f355ea2004570ccc6a6fb66a2c496096bf8036
Olivier <oliviertrahan>
2020-12-31 04:45:34 -0500

    clickable log cleanup

--- turboCo/ui/clickable.lua
+++ turboCo/ui/clickable.lua
@@ -46,10 +46,6 @@ local function create(args)
       x >= self.currentScreenPos.x and x <= maxClickablePosX and y == self.currentScreenPos.y --now we make sure it was this clickable which was clicked
     if wasClickedVal then
       logger.debug("clickable clicked: ", self.id, ", on posX: ", x, ", posY: ", y)
-      logger.debug("self.screenStartingPos.x: ", self.screenStartingPos.x,
-                  ", self.currentScreenPos.x: ", self.currentScreenPos.x,
-                  ", on self.screenBufferWidth: ", self.screenBufferWidth,
-                  ", self.screenBufferHeight: ", self.screenBufferHeight)
     end
     return wasClickedVal
   end

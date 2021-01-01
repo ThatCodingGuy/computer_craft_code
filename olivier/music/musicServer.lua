6ae4c83a7ac345b6b79427c86840f6c5f525bd90
Olivier <oliviertrahan>
2020-12-31 20:29:01 -0500

    fixed so removing debugging

--- olivier/music/musicServer.lua
+++ olivier/music/musicServer.lua
@@ -70,7 +70,6 @@ end
 local function getFileName(filePath)
   local name = fs.getName(filePath)
   local parts = lua_helpers.split(name, "%.")
-  logger.debug("file_parts: ", lua_helpers.to_string(parts))
   if #parts == 2 then
     name = parts[1]
   end

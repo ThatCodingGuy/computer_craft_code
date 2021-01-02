a1561e1c575af338f5b01c37c99d3e7cb5062637
Olivier <oliviertrahan>
2021-01-02 15:04:36 -0500

    testing logging

--- turboCo/logger.lua
+++ turboCo/logger.lua
@@ -13,6 +13,7 @@ local log_level_filter = LoggingLevel.WARNING
 --Set print_to_output to this to log to a file instead
 local log_to_file = function(...)
     local f = fs.open(log_file_path, 'a')
+    f.writeLine(lua_helpers.join(arg))
     table.remove(arg, #arg) --Removes the extra argument added by lua
     f.writeLine(lua_helpers.join(arg))
     f.close()
